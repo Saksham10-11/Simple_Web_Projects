4c31d15d8b2de2f593f68fd59f1c224024f6873d
Saksham <Sakshamsaipatwarpkd@gmail.com>
2024-02-21 18:33:59 +0530

    fix issues

--- Tic-Tac-Toe/index.js
+++ Tic-Tac-Toe/index.js
@@ -78,64 +78,78 @@ function gameControler(game) {
   const playerName2 = "Player2";
 
   const players = [];
-  players.push({ name: playerName1, value: 1 });
-  players.push({ name: playerName2, value: 0 });
+  players.push({
+    name: playerName1,
+    value: 1,
+    marker: "./icons/circle-svgrepo-com.svg",
+  });
+  players.push({
+    name: playerName2,
+    value: 0,
+    marker: "./icons/cross-svgrepo-com.svg",
+  });
 
   let turn = players[0];
 
   const showTurn = () => {
-    console.log(`${turn.name}'s turn ...`);
+    const element = document.querySelector(".main-heading");
+    element.innerHTML = `${turn.name}'s turn ...`;
   };
 
   const changeTurn = () => {
     turn = turn === players[0] ? players[1] : players[0];
   };
 
-  const placeToken = (i, j) => {
+  const placeToken = (id) => {
+    let i = parseInt(id.substr(0, 1));
+    let j = parseInt(id.substr(1, 1));
     game.getBoard()[i][j] = turn.value;
-  };
-
-  const printBoard = () => {
-    let board = game.getBoard();
-    console.log(board);
+    const cell = document.getElementById(id);
+    const html = `<img
+    src="${turn.marker}"
+    class="game-cell-img"
+    alt=""
+  />`;
+    cell.innerHTML = html;
   };
 
   const checkStatus = (costumJudge) => {
     let status = false;
-    if (costumJudge.gameOverStatus()) {
-      console.log("Game Over !!");
-    } else if (costumJudge.player1Status()) {
-      console.log("Player 1 has won ... ");
+    if (costumJudge.player1Status()) {
+      alert("Player 1 has won ... ");
+      location.reload();
     } else if (costumJudge.player2Status()) {
-      console.log("Player 2 has won ... ");
+      alert("Player 2 has won ... ");
+      location.reload();
+    } else if (costumJudge.gameOverStatus()) {
+      alert("Game Over !!");
+      location.reload();
     } else {
       status = true;
     }
     return status;
   };
 
-  return { changeTurn, placeToken, printBoard, showTurn, checkStatus };
+  return { changeTurn, placeToken, showTurn, checkStatus };
 }
 
 let game = gameBoard();
 let controller = gameControler(game);
 let costumJudge = judge(game);
 
-// let idx = 0;
-// while (controller.checkStatus(costumJudge)) {
-//   controller.printBoard();
-//   controller.showTurn();
-//   controller.placeToken(Math.floor(idx / 3), idx % 3);
-//   controller.changeTurn();
-//   idx++;
-// }
-// controller.printBoard();
+function handelClick(id) {
+  controller.placeToken(id);
+  controller.checkStatus(costumJudge);
+  controller.changeTurn();
+  controller.showTurn();
+}
 
 if (typeof document !== null) {
   const cells = document.querySelectorAll(".game-cell");
   for (let i = 0; i < cells.length; ++i) {
     cells[i].addEventListener("click", (event) => {
       console.log(event.target.id);
+      handelClick(event.target.id);
     });
   }
 }
